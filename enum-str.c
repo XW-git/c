e0e00bd051f8ef0ce786c652ef955cb9d18cd898
xian <xian@gmail.com>
2022-03-30 20:42:32 -0600

    add more stringlize tricks

--- enum-str.c
+++ enum-str.c
@@ -1,5 +1,9 @@
 #include "stdio.h"
 
+#define str(x) #x
+#define xstr(x) str(x)
+#define foo apple
+
 // prepare 
 #define FOREACH_FRUIT(FRUIT) \
         FRUIT(apple)   \
@@ -24,10 +28,33 @@ static const char *FRUIT_STRING[] = {
     FOREACH_FRUIT(GENERATE_STRING)
 };
 
+
+/* without second replace method */
+enum my_enum {
+	#define MY_ENUM_ITEM(_n, _v) _n = _v,
+	#include "enum-str.h"
+	#undef MY_ENUM_ITEM
+};
+
+const char *my_enum_to_string(enum my_enum e) {
+switch (e) {
+	#define MY_ENUM_ITEM(_n, _v) case _v: return #_n;
+	#include "enum-str.h"
+	#undef MY_ENUM_ITEM
+	default: return " not in the list\n\r";
+	}
+}
+
 int main() {
     int i;
     for( i = 0; i < 4; i++ ) {
         printf("enum %d as a string: %s\n", i, FRUIT_STRING[i]);
     }
-        
+
+    printf("%s\n", str(foo));    // convert to "foo"
+    printf("%s\n", xstr(foo));   // convert to str(apple), second pass, convert to "apple"
+
+    printf("%s = %d\n\r", my_enum_to_string(ABC), ABC );
+	printf("%s = %d\n\r", my_enum_to_string(FOO), FOO );
+
 }
